74e715dd2ac179a9ef60ded18566b1d3760fc4e0
Emmanuel <emmanuel.camposeco@hotmail.com>
2025-11-03 21:16:44 -0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', loadComponent: () => import('./presentation/pages/home/home').then(m => m.Home) },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', loadComponent: () => import('./presentation/pages/not-found/not-found').then(m => m.NotFound) }
+];
